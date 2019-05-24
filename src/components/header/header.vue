ee5799979e64ce9211af4b096c871a76d7a8f636
jun <413206188@qq.com>
2019-05-24 11:04:38 +0800

    first commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="header">
+    header
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+  data() {
+  return {
+
+  }
+  },
+  components: {
+    
+  }
+  }
+</script>
+
+<style scoped lang="less">
+</style>
--- src/router.js
+++ src/router.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  linkActiveClass:'active',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/goods',
+      name: 'goods',
+      component: () => import(/* webpackChunkName: "about" */ './components/goods/goods.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/ratings',
+      name: 'ratings',
+      component: () => import(/* webpackChunkName: "about" */ './components/ratings/ratings.vue')
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: () => import(/* webpackChunkName: "about" */ './components/seller/sellse.vue')
     }
   ]
 })
